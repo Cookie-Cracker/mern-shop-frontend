e7d537acc4ae3dad61b66efe41cfa6c0f24b7ae5
petit-restaurant <116051042+petit-restaurant@users.noreply.github.com>
2023-02-28 23:24:33 -0500

    Change baseURL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3900',
+    baseUrl: 'https://mern-shop-api.onrender.com',
+    // baseUrl: 'http://localhost:3900',
     credentials: 'include',
     // credentials: 'same-origin',
     prepareHeaders: (headers, { getState }) => {
